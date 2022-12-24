cd54d568387202b60be1b162b4548b20a211f8bd
victorfelipealvarezrojas <victorfelipealvarezrojas@hotmail.com>
2022-12-24 17:59:15 -0300

    agrego archivos restantes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
+export class DBConnection {
+
+    constructor() {
+        this.client = new mongoose.connect(process.env.BD_MONGO, { useNewUrlParser: true });
+    }
+
+    connect() {
+        return this.client;
+    }
+
+    disconnect() {
+        return this.client.close();
+    }
+}
---
+++ config/index.js
@@ -0,0 +1,1 @@
+export { DBConnection } from "./db";

95d1553a57f18b720ae9a767960c72ad4ed171f2
victorfelipealvarezrojas <victorfelipealvarezrojas@hotmail.com>
2022-12-24 23:41:28 -0300

    variables de entorno

---
+++ models/usuaurio.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const UsuariosShema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    apellido: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    creado: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('Usuario', UsuariosShema)
